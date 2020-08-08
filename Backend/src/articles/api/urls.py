c32218bb2a8f13daa90058525ec50e19907e5f8f
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-08 11:43:32 +0100

    rest_framework viewsets
    - Using rest_framework's viewsets to handle the redundant repetition of view classes in our views.py file.

--- Backend/src/articles/api/urls.py
+++ Backend/src/articles/api/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+# from django.urls import path
 
-from .views import ArticleListView, ArticleDetialView
+# # from .views import ArticleListView, ArticleDetialView, ArticleCreateView
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetialView.as_view())
-]
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('<pk>', ArticleDetialView.as_view())
+# ]
+
+
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, basename='articles')
+urlpatterns = router.urls
--- Backend/src/articles/api/views.py
+++ Backend/src/articles/api/views.py
@@ -1,14 +1,27 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import viewsets
 
 from articles.models import Article
 from .serializers import Article_serializer
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
     serializer_class = Article_serializer
+    queryset = Article.objects.all()
+
+# from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = Article_serializer
 
 
-class ArticleDetialView(RetrieveAPIView):
-    queryset = Article.objects.all()
-    serializer_class = Article_serializer
+# class ArticleDetialView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = Article_serializer
+
+
+# class ArticleCreateView(CreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = Article_serializer
