519e5720e4b2250d62e9d3ff3e98ff6c2f671831
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-12 23:50:07 +0100

    User authentication
    - Starting out by adding actions ( actionTypes) and reducers(auth) to your application

--- Frontend/gui/src/containers/Layout.js
+++ Frontend/gui/src/containers/Layout.js
@@ -10,8 +10,12 @@ const CustomLayout = (props) => {
       <Header>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">Posts</Menu.Item>
-          <Menu.Item key="2">Login</Menu.Item>
+          <Menu.Item key="1">
+            <Link to="/">Post</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/login">Login</Link>
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: "0 50px" }}>
---
+++ Frontend/gui/src/store/actions/auth.js
@@ -0,0 +1,38 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+import Password from "antd/lib/input/Password";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (token) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    token: token,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const authLogin = (username, password) => {
+  return (dispatch) => {
+    dispatch(authStart());
+
+    axios.post("http:127.0.0.1/rest-auth/login/", {
+      username: username,
+      password: password,
+    })
+    .then(res => {
+        const token = res.data.key,
+        const expirationDate = new Data(new Date().getTime() + 3)
+    })
+  };
+};
