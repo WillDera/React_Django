468588bfb4d74f19b6256e5cbe04cf5c153187ea
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-16 21:39:03 +0100

    Signup
    - Creating the signup page for the client side

---
+++ Frontend/gui/src/containers/Signup.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import {
+  Form,
+  Input,
+  Tooltip,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+
+const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option;
+
+const Signup = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <Form
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        residence: ["zhejiang", "hangzhou", "xihu"],
+        prefix: "86",
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Username!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!",
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          type="mail"
+          placeholder="Email"
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password!",
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject(
+                "The two passwords that you entered do not match!"
+              );
+            },
+          }),
+        ]}
+      >
+        <Input.Password
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Confirm Password"
+        />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Signup
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Signup;
